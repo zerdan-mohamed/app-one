70689f7d1585d17baebfefb7070d88f270169dfc
zerdan-mohamed <zerdan.mohamed@gmail.com>
2019-02-21 17:32:54 +0100

    add home and contacts components, update about component, create app main routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {AboutComponent} from './about/about.component';
 import {AboutService} from '../services/about.service';
+import {RouterModule, Routes} from '@angular/router';
+import {ContactsComponent} from './contacts/contacts.component';
+import {HomeComponent} from './home/home.component';
+
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: 'contacts', component: ContactsComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    ContactsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [AboutService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

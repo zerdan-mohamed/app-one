b316b4bb512d6da266ccee0094e6eee2646f2dfc
zerdan-mohamed <zerdan.mohamed@gmail.com>
2019-02-21 15:17:04 +0100

    add about service and bootstrap configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {AboutComponent} from './about/about.component';
+import {AboutService} from '../services/about.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [AboutService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/services/about.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class AboutService {
+  user = {
+    nom: 'mohamed',
+    email: 'med@gmail.com',
+    tel: '123456789'
+  }
+
+  messages = [
+    {date: new Date(), txt: 'message n1'},
+    {date: new Date(), txt: 'message n2'},
+    {date: new Date(), txt: 'message n3'}
+  ];
+
+  addMessage(m) {
+    m.date = new Date();
+    this.messages.push(m);
+  }
+
+  getAllMessages() {
+    return this.messages;
+  }
+
+  getUser() {
+    return this.user;
+  }
+}

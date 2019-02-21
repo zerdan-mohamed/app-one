91efb3d5dc6e574255877bda5ebbf963f2d8ad12
zerdan-mohamed <zerdan.mohamed@gmail.com>
2019-02-21 15:15:40 +0100

    add about component

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {AboutService} from '../../services/about.service';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+  user: any;
+  messages = [];
+
+  message = {
+    date: null,
+    txt: ''
+  };
+
+  constructor(private aboutService: AboutService) {
+    this.user = this.aboutService.getUser();
+    this.messages = this.aboutService.getAllMessages();
+  }
+
+  ngOnInit() {
+  }
+
+  onAddMessage(m) {
+    /*console.log(c);*/
+    this.aboutService.addMessage(m);
+    /*this.message.txt = '';*/
+    this.messages = this.aboutService.getAllMessages();
+  }
+}
